79a9979c95dca45c590a5932039c2d7284178c17
Chris Denmark <chrisdanish@googlemail.com>
2022-06-02 17:35:41 +0100

    html within php tag

--- helloworld/index.php
+++ helloworld/index.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Hello World!</title>
-    </head>
-    <body>
-        
-        <?php 
-        echo("Hello World!");
-        ?>
-        
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <title>Hello World!</title>
+</head>
+
+<body>
+
+    <?php
+    echo "<h1>HTML within quotation marks within php</h1>";
+    echo ("Hello World!");
+    ?>
+
+</body>
+
 </html>
