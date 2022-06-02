bb1aecf8c2b29e84f57ba3d5ca00ae7cabba9c45
Chris Denmark <chrisdanish@googlemail.com>
2022-06-02 18:20:15 +0100

    add a selection of php string functions

--- strings/strings.php
+++ strings/strings.php
@@ -10,6 +10,24 @@
 
 <body>
     <?php
+    echo "This is a string!<br>";
+    $phrase = "This is another string<br>";
+    echo $phrase;
+
+    echo strtolower($phrase);
+    echo strtoupper($phrase);
+    echo strlen($phrase);
+    echo $phrase[0];
+
+    echo $phrase[0] = "t";
+    echo $phrase;
+
+    echo str_replace("this", "that", $phrase);
+    echo $phrase;
+
+    echo substr($phrase, 16, 6);
+
+    echo "<br><hr>PHP STRING FUNCTIONS FOR MORE INFO"
 
     ?>
 </body>
