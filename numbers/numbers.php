c5dfd98246d7501f71b98c301242762b765188d6
Chris Denmark <chrisdanish@googlemail.com>
2022-06-03 10:53:13 +0100

    Add number examples and functions

--- numbers/numbers.php
+++ numbers/numbers.php
@@ -1,1 +1,53 @@
-!
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Numbers</title>
+</head>
+
+<body>
+    <?php
+
+    echo 40;
+    echo -40;
+    echo 40.9;
+
+    echo 5 + 9 - 2 * 10 / 4;
+
+    echo 10 % 3;
+    echo 4 + 5 * 10;
+    echo (4 + 5) * 10;
+
+    $num = 10;
+    echo $num;
+
+    $num++;
+    echo $num;
+    $num--;
+    echo $num;
+    $num += 25;
+    echo $num;
+    $num *= 35;
+    echo $num;
+
+    echo abs(-100);
+    echo pow(2, 4);
+    echo sqrt(144);
+    echo max(2, 10);
+    echo min(2, 10);
+    echo round(4.32);
+    echo ceil(4.2);
+    echo floor(3.9);
+
+    echo "<br><hr>Math functions";
+
+
+
+    ?>
+
+</body>
+
+</html>
