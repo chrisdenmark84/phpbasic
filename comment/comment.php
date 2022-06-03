f1a89c007f875d0975fb3cc9ad1efdd4a93ab200
Chris Denmark <chrisdanish@googlemail.com>
2022-06-03 20:41:59 +0100

    Add some comments

---
+++ comment/comment.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments!</title>
+</head>
+
+<body>
+
+    <?php
+    //Single line comment
+    echo "Comment is fun";
+
+    /* This
+    is a comment
+    block
+    */
+
+    ?>
+
+
+</body>
+
+</html>
