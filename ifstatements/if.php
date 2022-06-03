f5123eb817f4fc86aed9c1a8fed3fe9ddc69b24d
Chris Denmark <chrisdanish@googlemail.com>
2022-06-03 17:52:16 +0100

    Add if statements

--- ifstatements/if.php
+++ ifstatements/if.php
@@ -13,6 +13,31 @@
 
     <?php
 
+    $isMale = false;
+    $isTall = false;
+
+    if ($isMale && $isTall) {
+        echo "You are a tall male<br>";
+    } else {
+        echo "You are not a tall male<br>";
+    }
+
+
+    if ($isMale || $isTall) {
+        echo "You are tall or a male<br>";
+    } else {
+        echo "You are not tall or a male<br>";
+    }
+
+    if ($isMale && $isTall) {
+        echo "You are a tall male<br>";
+    } elseif ($isMale && !$isTall) {
+        echo "You are a short male<br>";
+    } elseif (!$isMale && $isTall) {
+        echo "You are not male but tall<br>";
+    } else {
+        echo "You are not male and not tall";
+    }
 
     ?>
 
