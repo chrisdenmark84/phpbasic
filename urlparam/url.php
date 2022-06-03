707c3a879396d76205561bd2cd40435363a13077
Chris Denmark <chrisdanish@googlemail.com>
2022-06-03 13:31:26 +0100

    Add URL.php and create form and output

---
+++ urlparam/url.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>URL Paramters</title>
+</head>
+
+<body>
+
+    <form action="url.php" method="GET">
+
+        Name: <input type="text" name="fname">
+        <input type="submit">
+
+    </form>
+
+    <?php
+
+    echo $_GET['fname'];
+
+    ?>
+
+</body>
+
+</html>
