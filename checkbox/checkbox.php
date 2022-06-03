edf983c44ea7b3890c36f07d992f4807010a603f
Chris Denmark <chrisdanish@googlemail.com>
2022-06-03 14:08:57 +0100

    Add checkbox.php and add basic form

---
+++ checkbox/checkbox.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checkbox</title>
+</head>
+
+<body>
+
+    <form action="checkbox.php" method="POST">
+
+
+        <input type="submit">
+    </form>
+
+    <?php
+
+
+
+    ?>
+
+
+</body>
+
+</html>
