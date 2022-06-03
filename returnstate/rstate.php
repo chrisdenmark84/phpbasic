0049fb6099218c382f758f2228a764968f81a47b
Chris Denmark <chrisdanish@googlemail.com>
2022-06-03 17:24:00 +0100

    Add cube function, call via user input & return value

--- returnstate/rstate.php
+++ returnstate/rstate.php
@@ -1,1 +1,36 @@
-!
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Return Statments</title>
+</head>
+
+<body>
+
+    <form action="rstate.php" method="POST">
+        <input type="number" name="cubeInput">
+        <input type="submit">
+    </form>
+
+
+    <?php
+
+    function cube($num)
+    {
+        return $num * $num * $num;
+    }
+
+    $cubeResult = cube($_POST["cubeInput"]);
+    echo $cubeResult;
+
+    echo cube($_POST["cubeInput"]);
+
+    ?>
+
+
+</body>
+
+</html>
