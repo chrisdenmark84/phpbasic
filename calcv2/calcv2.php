1e85facba37b5487b0655b30677725e418df1f4b
Chris Denmark <chrisdanish@googlemail.com>
2022-06-03 18:24:28 +0100

    Assigned user input to variable and create if statement for operators

--- calcv2/calcv2.php
+++ calcv2/calcv2.php
@@ -11,13 +11,29 @@
 <body>
 
     <form action="calcv2.php" method="POST">
-        First Number: <input type="number" name="num1"> <br>
-        Second Number: <input type="number" name="num2"> <br>
+        First Number: <input type="number" step="0.01" name="num1"> <br>
         Type of Math: <input type="text" name="op"> <br>
+        Second Number: <input type="number" step="0.01" name="num2"> <br>
         <input type="submit">
     </form>
 
     <?php
+
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $op = $_POST['op'];
+
+    if ($op == "+") {
+        echo $num1 + $num2;
+    } elseif ($op == "-") {
+        echo $num1 - $num2;
+    } elseif ($op == "*") {
+        echo $num1 * $num2;
+    } elseif ($op == "/") {
+        echo $num1 / $num2;
+    } else {
+        echo "Invalid Type of Math. Enter either +, -, * or /";
+    }
 
 
     ?>
